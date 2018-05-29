5446508ac0a918684c4f74da06260b0fa961c09e
Katineto <not.a.mielikki@gmail.com>
2018-05-29 22:42:57 +0800

    Rename files

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
-const canvas = document.querySelector('#container')
-const clearBtn = document.querySelector('#clear')
-const gradientBtn = document.querySelector('#gradient')
-let lastSize = 16
-
+const defaultSize = 16
+const container = document.querySelector('.grid-container')
+function resetContainer () {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
 function makeGrid(size) {
+    resetContainer()
     for(let i = 0; i < (size*size); i ++) {
         const oneSquare = document.createElement('div')
         oneSquare.className = 'square'
-        oneSquare.style.height = `${Math.round(800 / size)}px`
-        oneSquare.style.width = `${Math.round(800 / size)}px`
-        canvas.appendChild(oneSquare)
-    } 
-    lastSize = size*size
-}
-function draw(e) {
-    if(e.target.className == 'square') {
-        e.target.style.backgroundColor = '#036'
+        container.appendChild(oneSquare)
     }
-}
-function newGrid() {
-    //clear the grid
-    if(canvas.childElementCount > 0) {
-        for(let i = 0; i < lastSize; i++) {
-            const squareToRemove = document.querySelector('div[class="square"]')
-            canvas.removeChild(squareToRemove)
-        }
-    }
-    //use promt
-    const answer = window.prompt(`Let's make new grid! How many squares per side?`)
-    answer.trim()
-    const answerToNumber = Number(answer)
-    if(answerToNumber > 64) {
-        window.alert(`That's too big, try again.`)
-        return
-    }   
-    //create new grid
-    makeGrid(answer)
-}
-function handleGradient() {
-    
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)` 
 }
 
-canvas.addEventListener('mousemove', draw)
-clearBtn.addEventListener('click', newGrid)
-gradientBtn.addEventListener('click', handleGradient)
+const rawInput = document.querySelector('input')
+const form = document.getElementById('form')
+function handleInput(e) {
+    const rawToNumber = Number(rawInput.value)
+    makeGrid(rawToNumber)
+    e.preventDefault()
+    form.reset()
+}
+let lastTarget
+function changeOpacity(e){
+    if(e.target.className == 'square' && e.target != lastTarget) {
+        e.target.style.opacity = `${Number(e.target.style.opacity) + 0.3}`
+        lastTarget = e.target
+    }
+}
+
+container.addEventListener('mousemove', changeOpacity)
+form.addEventListener('submit', handleInput)
 window.onload = function() {
-    makeGrid(16)
+    makeGrid(defaultSize)
 }
